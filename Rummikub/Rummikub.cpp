3f83b602901e3834ee148903ede941f6696a67c0
Victor Agovski <viktorgbs4@gmail.com>
2026-01-16 22:34:23 +0200

    Added functionality for combinations and finding winner

--- Rummikub/Rummikub.cpp
+++ Rummikub/Rummikub.cpp
@@ -23,6 +23,16 @@ struct Player {
     int currentHandSize;
 };
 
+struct Combination {
+    Tile tiles[TILE_SIZE];
+    int size;
+};
+
+struct Table {
+    Combination combinations[TILE_SIZE];
+    int combinationsCount;
+};
+
 // ------------------------------------------------ Functions Declarations ------------------------------------------------
 
 void enterValidPlayersCount(int& playersCount);
@@ -38,6 +48,33 @@ char colorToChar(Color color);
 void printTile(const Tile& tile);
 void printHand(const Player& player);
 void printAllHands(const Player* players, const int playersCount);
+
+bool isJoker(const Tile& tile);
+
+bool isValidGroup(const Tile* tiles, int size);
+
+void sortArrayFunc(int* array, int size);
+bool isValidRun(const Tile* tiles, int size);
+
+bool isValidCombination(const Tile* tiles, int size);
+
+void printHandWithIndexes(const Player& player);
+bool removeFromHand(Player& player, int index);
+void addCombinationToTable(Table& table, const Tile* tiles, int size);
+void printTable(const Table& table);
+
+bool readChosenIndexes(int* chosenIndexes, int chosenCount, int handSize);
+void buildChosenTiles(const Player& player, const int* chosenIndexes, int chosenCount, Tile* chosenTiles);
+void sortIndexesDesc(int* array, int size);
+void removeChosenFromHand(Player& player, int* chosenIndexes, int chosenCount);
+
+bool tryPlayCombinationTurn(Player& player, Table& table, int playerNumber);
+
+int tilePoints(const Tile& tile);
+int handPoints(const Player& player);
+
+bool drawOneTile(Player& player, Tile* pile, int& pileSize);
+int findWinnerByLowestPoints(Player* players, int playersCount);
 
 // ------------------------------------------------ main() Function ------------------------------------------------
 
@@ -60,6 +97,44 @@ int main()
 
     cout << "Remaining tiles in pile: " << pileSize << endl;
 
+    Table table;
+    table.combinationsCount = 0;
+
+    bool gameOver = false;
+
+    while (!gameOver) {
+        for (int i = 0; i < playersCount; i++) {
+            cout << endl << "==============================" << endl;
+            printTable(table);
+
+            bool played = tryPlayCombinationTurn(players[i], table, i + 1);
+
+            cout << endl << "Player " << (i + 1) << " new hand:" << endl;
+            printHandWithIndexes(players[i]);
+
+            if (players[i].currentHandSize == 0) {
+                cout << endl << "Player " << (i + 1) << " wins!" << endl;
+                gameOver = true;
+                break;
+            }
+
+            if (!played) {
+                cout << "No combination played. You must draw 1 tile." << endl;
+
+                if (!drawOneTile(players[i], pile, pileSize)) 
+                {
+                    cout << endl << "Pile is empty. Game ends." << endl;
+                    int winnerIndex = findWinnerByLowestPoints(players, playersCount);
+
+                    cout << "Winner is Player " << (winnerIndex + 1) << " with " << handPoints(players[winnerIndex]) << " points." << endl;
+
+                    gameOver = true;
+                    break;
+                }
+            }
+        }
+    }
+
     return 0;
 }
 
@@ -79,10 +154,7 @@ void enterValidPlayersCount(int& playersCount) {
             continue;
         }
 
-        if (playersCount >= 2 && playersCount <= 4) 
-        {
-            break;
-        }
+        break;
     }
 }
 
@@ -218,4 +290,417 @@ void printAllHands(const Player* players, const int playersCount) {
         cout << "Player " << (i + 1) << " hand:" << endl;
         printHand(players[i]);
     }
+}
+
+bool isJoker(const Tile& tile) {
+    if (tile.value == 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool isValidGroup(const Tile* tiles, int size) {
+    if (size != 3 && size != 4) 
+    {
+        return false;
+    }
+
+    const int COLORS_COUNT = 4;
+    int targetValue = -1;
+
+    for (int i = 0; i < size; i++) 
+    {
+        const Tile& currentTile = tiles[i];
+        if (!isJoker(currentTile))
+        {
+            targetValue = currentTile.value;
+            break;
+        }
+    }
+
+    if (targetValue == -1) {
+        return true;
+    }
+
+    bool usedColors[COLORS_COUNT] = { false, false, false, false };
+
+    for (int i = 0; i < size; i++) 
+    {
+        const Tile& currentTile = tiles[i];
+
+        if (isJoker(currentTile))
+        {
+            continue;
+        }
+
+        if (currentTile.value != targetValue)
+        {
+            return false;
+        }
+
+        if (usedColors[(int)currentTile.color])
+        {
+            return false;
+        }
+        usedColors[(int)currentTile.color] = true;
+    }
+
+    return true;
+}
+
+void sortArrayFunc(int* array, int size) {
+    if (array == nullptr || size <= 1)
+    {
+        return;
+    }
+
+    for (int i = 0; i < size - 1; i++) 
+    {
+        for (int j = 0; j < size - i - 1; j++) 
+        {
+            if (array[j] > array[j + 1]) 
+            {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
+
+bool isValidRun(const Tile* tiles, int size) {
+    if (tiles == nullptr) 
+    {
+        return false;
+    }
+    if (size < 3) 
+    {
+        return false;
+    }
+
+    bool hasNonJoker = false;
+    Color runColor = ORANGE;
+
+    for (int i = 0; i < size; i++) 
+    {
+        const Tile& currentTile = tiles[i];
+
+        if (!isJoker(currentTile)) 
+        {
+            runColor = currentTile.color;
+            hasNonJoker = true;
+            break;
+        }
+    }
+
+    if (!hasNonJoker)
+    {
+        return true;
+    }
+
+    int values[TILE_SIZE];
+    int valuesCount = 0;
+    int jokersCount = 0;
+
+    for (int i = 0; i < size; i++) 
+    {
+        const Tile& currentTile = tiles[i];
+
+        if (isJoker(currentTile)) 
+        {
+            jokersCount++;
+            continue;
+        }
+
+        if (currentTile.color != runColor) 
+        {
+            return false;
+        }
+
+        values[valuesCount++] = currentTile.value;
+    }
+
+    sortArrayFunc(values, valuesCount);
+
+    for (int i = 1; i < valuesCount; i++)
+    {
+        if (values[i] == values[i - 1]) 
+        {
+            return false;
+        }
+    }
+
+    int neededJokers = 0;
+    for (int i = 1; i < valuesCount; i++) 
+    {
+        int gap = values[i] - values[i - 1] - 1;
+
+        if (gap < 0) 
+        {
+            return false;
+        }
+        neededJokers += gap;
+    }
+
+    if (neededJokers <= jokersCount)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+bool isValidCombination(const Tile* tiles, int size) {
+    return isValidGroup(tiles, size) || isValidRun(tiles, size);
+}
+
+void printHandWithIndexes(const Player& player) {
+    for (int i = 0; i < player.currentHandSize; i++) 
+    {
+        cout << i << ": ";
+        printTile(player.hand[i]);
+        cout << endl;
+    }
+}
+
+bool removeFromHand(Player& player, int index) {
+    if (index < 0 || index >= player.currentHandSize) 
+    {
+        return false;
+    }
+
+    for (int i = index; i < player.currentHandSize - 1; i++) 
+    {
+        player.hand[i] = player.hand[i + 1];
+    }
+    player.currentHandSize--;
+
+    return true;
+}
+
+void addCombinationToTable(Table& table, const Tile* tiles, int size) {
+    if (table.combinationsCount >= TILE_SIZE) 
+    {
+        return;
+    }
+
+    table.combinations[table.combinationsCount].size = size;
+
+    for (int i = 0; i < size; i++) 
+    {
+        table.combinations[table.combinationsCount].tiles[i] = tiles[i];
+    }
+    table.combinationsCount++;
+}
+
+void printTable(const Table& table) {
+    cout << endl << "TABLE:" << endl;
+
+    for (int i = 0; i < table.combinationsCount; i++) 
+    {
+        cout << i << ") ";
+
+        for (int j = 0; j < table.combinations[i].size; j++) 
+        {
+            printTile(table.combinations[i].tiles[j]);
+            cout << " ";
+        }
+
+        cout << endl;
+    }
+}
+
+bool readChosenIndexes(int* chosenIndexes, int chosenCount, int handSize) {
+    for (int i = 0; i < chosenCount; i++) 
+    {
+        cin >> chosenIndexes[i];
+
+        if (cin.fail()) 
+        {
+            cin.clear(); 
+            cin.ignore(10000, '\n'); 
+            return false; 
+        }
+
+        if (chosenIndexes[i] < 0 || chosenIndexes[i] >= handSize) 
+        {
+            return false;
+        }
+
+        for (int j = 0; j < i; j++) 
+        {
+            if (chosenIndexes[i] == chosenIndexes[j]) 
+            {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+void buildChosenTiles(const Player& player, const int* chosenIndexes, int chosenCount, Tile* chosenTiles) {
+    for (int i = 0; i < chosenCount; i++) 
+    {
+        chosenTiles[i] = player.hand[chosenIndexes[i]];
+    }
+}
+
+void sortIndexesDesc(int* array, int size) {
+    if (array == nullptr || size <= 1)
+    {
+        return;
+    }
+
+    for (int i = 0; i < size - 1; i++) 
+    {
+        for (int j = 0; j < size - i - 1; j++) 
+        {
+            if (array[j] < array[j + 1])
+            {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void removeChosenFromHand(Player& player, int* chosenIndexes, int chosenCount) {
+    sortIndexesDesc(chosenIndexes, chosenCount);
+
+    for (int i = 0; i < chosenCount; i++) 
+    {
+        removeFromHand(player, chosenIndexes[i]);
+    }
+}
+
+bool tryPlayCombinationTurn(Player& player, Table& table, int playerNumber) {
+    cout << endl << "Player " << playerNumber << " turn." << endl;
+    cout << "1) Play a new combination" << endl;
+    cout << "2) Draw a tile (end turn)" << endl;
+    cout << "Choose: ";
+
+    int choice;
+    cin >> choice;
+
+    if (cin.fail()) 
+    {
+        cin.clear();
+        cin.ignore(10000, '\n');
+        cout << "Invalid input." << endl;
+        return false;
+    }
+
+    if (choice == 2)
+    {
+        return false;
+    }
+
+    if (choice != 1) 
+    {
+        cout << "Invalid choice." << endl;
+        return false;
+    }
+
+    cout << endl << "Player " << playerNumber << ", choose tiles to form a combination." << endl;
+    printHandWithIndexes(player);
+
+    cout << "How many tiles in the combination? " << endl;
+    int chosenCount;
+    cin >> chosenCount;
+
+    if (cin.fail()) 
+    {
+        cin.clear();
+        cin.ignore(10000, '\n');
+        cout << "Invalid input.\n";
+        return false;
+    }
+
+    if (chosenCount < 3 || chosenCount > HAND_SIZE || chosenCount > player.currentHandSize) 
+    {
+        cout << "Invalid count." << endl;
+        return false;
+    }
+
+    int chosenIndexes[HAND_SIZE];
+    Tile chosenTiles[HAND_SIZE];
+
+    cout << "Enter " << chosenCount << " indexes: ";
+
+    if (!readChosenIndexes(chosenIndexes, chosenCount, player.currentHandSize)) 
+    {
+        cout << "Invalid indexes (out of range, duplicates, or bad input)." << endl;
+        return false;
+    }
+
+    buildChosenTiles(player, chosenIndexes, chosenCount, chosenTiles);
+
+    if (!isValidCombination(chosenTiles, chosenCount)) 
+    {
+        cout << "Invalid combination!" << endl;
+        return false;
+    }
+
+    cout << "Valid combination! Adding to table..." << endl;
+    addCombinationToTable(table, chosenTiles, chosenCount);
+    removeChosenFromHand(player, chosenIndexes, chosenCount);
+
+    return true;
+}
+
+int tilePoints(const Tile& tile) {
+    if (isJoker(tile)) 
+    { 
+        return 30; 
+    }
+
+    return tile.value;
+}
+
+int handPoints(const Player& player) {
+    int sum = 0;
+
+    for (int i = 0; i < player.currentHandSize; i++) 
+    {
+        sum += tilePoints(player.hand[i]);
+    }
+
+    return sum;
+}
+
+bool drawOneTile(Player& player, Tile* pile, int& pileSize) 
+{
+    Tile drawn;
+
+    if (!drawFromPile(pile, pileSize, &drawn)) 
+    {
+        return false;
+    }
+
+    addToHand(&player, drawn);
+
+    cout << "You drew: ";
+    printTile(drawn);
+    cout << endl;
+
+    return true;
+}
+
+int findWinnerByLowestPoints(Player* players, int playersCount) {
+    int bestIndex = 0;
+    int bestPoints = handPoints(players[0]);
+
+    for (int i = 1; i < playersCount; i++) 
+    {
+        int points = handPoints(players[i]);
+        if (points < bestPoints)
+        {
+            bestPoints = points;
+            bestIndex = i;
+        }
+    }
+    return bestIndex;
 }
